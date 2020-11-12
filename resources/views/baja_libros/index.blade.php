32e5edd32838db56819647d11618905021ffedd7
joaquinpotocki <joaquinpotocki@gmail.com>
2020-11-12 19:59:03 -0300

    Ultimo commit - Part 5

---
+++ resources/views/baja_libros/index.blade.php
@@ -0,0 +1,81 @@
+@extends('admin-lte.index')
+
+@section('content')
+
+<div class="card">
+    <div class="card-header">Tabla de bajas
+        <a class="btn btn-primary btn-sm float-right text-white" href="{{route('baja_libros.create')}}">Nuevo</a>
+    </div>
+    <div class="card-body">
+        <table id="datatable" class="table table-striped table-bordered dataTable">
+            <thead>
+                <tr>
+                    <th scope="col">Fecha de Ingreso</th>
+                    <th scope="col">Libro/editorial</th>
+                    <th scope="col">Cantidad</th>
+                    <th scope="col">Tipo de baja</th>
+                    <th scope="col">Opciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($baja_libros as $baja_libro)
+                <tr>
+                    <td>{{$baja_libro->fecha_baja}}</td>
+                    <td>{{$baja_libro->libro->nombre}} - {{$baja_libro->libro->editoriales->nombre_editorial}}</td>
+                    <td>{{$baja_libro->cantidad}}</td>
+                    <td>{{$baja_libro->tipo_bajas->nombre_baja}}</td>
+                    <td class="text-right">
+                        <a class="btn btn-danger btn-sm text-white delete" val-palabra={{$baja_libro->id}}>Borrar</a>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+
+</div>
+<div id="confirmDelete" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h2 class="modal-title">Confirmacion</h2>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+            <div class="modal-body">
+                <h4 align="center" style="margin:0;">¿Esta seguro que desea borrarlo?</h4>
+            </div>
+            <div class="modal-footer">
+                <form id="formDelete" action="" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    {{-- Paso el id de la materia  aborrar en materia_delete--}}
+                    <button type="submit" name="ok_delete" id="ok_delete" class="btn btn-danger">SI Borrar</button>
+                </form>
+                <button type="button" class="btn btn-default" data-dismiss="modal">NO Borrar</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+@endsection
+
+@push('scripts')
+
+{{-- Script para eliminar --}}
+<script>
+    $(document).on('click', '.delete', function(){
+    id = $(this).attr('val-palabra');
+
+    url2="{{route('baja_libros.destroy',":id")}}";
+    url2=url2.replace(':id',id);
+ 
+    $('#formDelete').attr('action',url2);
+    $('#confirmDelete').modal('show');
+    });
+
+    $('#formDelete').on('submit',function(){
+    $('#ok_delete').text('Eliminando...')
+    });
+</script>
+@endpush
