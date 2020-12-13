cf38f12b4090749d22ce6bbd5d3206bfc916e757
joaquinpotocki <joaquinpotocki@gmail.com>
2020-12-13 03:18:56 -0300

    Show de lectores
    Se muestra un modulo inteligente

---
+++ resources/views/lectores/show.blade.php
@@ -0,0 +1,72 @@
+@extends('admin-lte.index')
+
+@section('content')
+
+<div class="row">
+    <div class="col-md-9">
+        <div class="card">
+            <div class="card-header p-12">
+                <ul class="nav nav-pills">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#detalleLibro" data-toggle="tab">Detalle</a>
+                    </li>
+                </ul>
+            </div><!-- /.card-header -->
+            <div class="card-body">
+                <div class="tab-content">
+                    <div class="active tab-pane" id="detalleLibro">
+
+                        <div class="text-muted" style="font-family: 'Open Sans', serif;">Lector:  {{$lector->nombres}} {{ $lector->apellidos}}</div>
+                        <hr style="margin-bottom: 1%; margin-top: 0%">
+                            <h4>La reputacion de este lector es: 
+                            @if ($lector->getreputacion($lector->reputacion) == 'MUY BUENO')
+                                    <span class="badge badge-success" style="color: white; ">{{ $lector->getreputacion($lector->reputacion) }}</span>
+                                @endif 
+                                @if ($lector->getreputacion($lector->reputacion) == 'BUENO')
+                                    <span class="badge badge-success" style="color: white; ">{{ $lector->getreputacion($lector->reputacion) }}</span>
+                                @endif 
+                                @if ($lector->getreputacion($lector->reputacion) == 'MEDIO')
+                                    <span class="badge badge-warning" style="color: white; ">{{ $lector->getreputacion($lector->reputacion) }}</span>
+                                @endif 
+                                @if ($lector->getreputacion($lector->reputacion) == 'MALO')
+                                    <span class="badge badge-danger" style="color: white; ">{{ $lector->getreputacion($lector->reputacion) }}</span>
+                                @endif 
+                                @if ($lector->getreputacion($lector->reputacion) == 'MUY MALO')
+                                    <span class="badge badge-danger" style="color: white; ">{{ $lector->getreputacion($lector->reputacion) }}</span>
+                                @endif 
+                            </h4>
+
+                        
+                        {{-- <dd>{{$lector->getreputacion($lector->reputacion)}}</dd> --}}
+
+                        
+                        <br>
+                        <br>
+                        <dl class="row" style="margin-left: 1%">
+                            <dt class="col-sm-3">CUIL</dt>
+                            <dd class="col-sm-8 text-muted">{{ $lector->cuil }}</dd>
+                        </dl>
+                        <dl class="row" style="margin-left: 1%">
+                            <dt class="col-sm-3">Email</dt>
+                            <dd class="col-sm-8 text-muted">{{ $lector->email }}</dd>
+                        </dl>
+                        </dl>
+                    </div>
+                </div>
+                <!-- /.tab-content -->
+            </div><!-- /.card-body -->
+
+        </div>
+        <!-- /.nav-tabs-custom -->
+    </div>
+    <!-- /.col -->
+</div>
+<!-- /.row -->
+
+
+
+@endsection
+
+@push('scripts')
+
+@endpush
