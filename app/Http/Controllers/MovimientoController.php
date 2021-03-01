5cb073a581e1c5d1046282023c4e3fa7b6b8432c
joaquinpotocki <joaquinpotocki@gmail.com>
2021-03-01 02:08:44 -0300

    ControladoresDos
    Se realizo cambios en dos controladores

--- app/Http/Controllers/MovimientoController.php
+++ app/Http/Controllers/MovimientoController.php
@@ -10,6 +10,8 @@ use Exception;
 use App\Estado;
 use App\EstadoDevolucion;
 use App\Configuracion;
+use App\BajaLibro;
+
 
 use Illuminate\Support\Carbon;
 
@@ -27,10 +29,11 @@ class MovimientoController extends Controller
         $movimientos = Movimiento::all();
         $configuracion = Configuracion::first() ;
         $lectores = Lector::all();
+        $bajas = BajaLibro::all();
 
       
 
-        return view('movimientos.index', compact('movimientos','libros','configuracion', 'estados', 'lectores'));
+        return view('movimientos.index', compact('movimientos', 'bajas','libros','configuracion', 'estados', 'lectores'));
     }
 
     /**
@@ -71,54 +74,67 @@ class MovimientoController extends Controller
      */
     public function store(Request $request)
     {
-        // try{
+        try{
+            $lector = Lector::find($request->lector_id);
             for ($i = 0; $i < sizeof($request->cantidad); $i++){
                 // if (($request->libros_select_id[$i]) && ($request->cantidad[$i]) == null){
                 //     return redirect(route('ingreso_libros.index'))->with('success','TU VIEJA!'); 
                 // }
+                
                 $libro = Libro::find($request->libros_select_id[$i]);
                 
-                if ($libro->stock_fantasma = 0){
-                    return redirect(route('movimientos.create'))->with('error','No contamos con ejemplares disponibles!'); 
+                if ($libro->stock_fantasma == 0){
+                    return redirect(route('movimientos.index'))->with('error','No contamos con los ejemplares correspondientes !'); 
+                    
                 }else{
-                    
-                    $movimiento = new Movimiento();
-                
-                    $movimiento->fecha_prestamo = $request->fecha_prestamo;  
-                    $movimiento->lector_id = $request->lector_id;  
-                    $movimiento->cantidad = $request->cantidad[$i];
-                    // $date = Carbon::create($request->fecha_prestamo)->addWeek()->format('Y-m-d'); 
                     $config = Configuracion::first();
-                    $movimiento->fecha_devolucion = Carbon::create($request->fecha_prestamo)->addWeek($config->semana_prestamo)->format('Y-m-d'); 
-                    
-                    $movimiento->fecha_devolucion_real = $request->fecha_prestamo;  
-        
-                    $movimiento->estado_id = 1;
-                    $movimiento->estado_devolucion_id = 1;
-                    
-                    $movimiento->save();
-                    // $movimiento->libros->sync($request->libros_select_id[$i]);
-                    $movimiento->libro()->attach($request->libros_select_id[$i]); //Para poder entrar a una tabla intermedia
-                    // $movimiento->libros->attach($request->libros_select_id[$i]) ;
-                    // return $request;
-                    //Update realizado en la tabla libros (se sumaron la cantidad que ingreso de este libro)
-        
-                    $libro = Libro::find($request->libros_select_id[$i]);
-                    
-                    if($libro->stock_fantasma > 0 && $libro->stock_fantasma > $request->cantidad[$i] ){
-                        $libro->stock_fantasma -= $request->cantidad[$i]; //Aqui sumamos tambien asumamos el stock de llibro para que se sumen a los prestados
-                        $libro->update();
+                    if(($lector->cantidadPrestamo) < ($config->control_prestamo)){
+                        $movimiento = new Movimiento();
+                
+                        $movimiento->fecha_prestamo = $request->fecha_prestamo;  
+                        $movimiento->lector_id = $request->lector_id;  
+                        $movimiento->cantidad = $request->cantidad[$i];
+                        // $date = Carbon::create($request->fecha_prestamo)->addWeek()->format('Y-m-d'); 
+                        // $config = Configuracion::first();
+                        $movimiento->fecha_devolucion = Carbon::create($request->fecha_prestamo)->addWeek($config->semana_prestamo)->format('Y-m-d'); 
+                        
+                        $movimiento->fecha_devolucion_real = $request->fecha_prestamo;  
+            
+                        $movimiento->estado_id = 1;
+                        $movimiento->estado_devolucion_id = 1;
+    
+                        $lector->cantidadPrestamo += 1;
+                        $lector->update();
+                        
+                        
+                        // $movimiento->libros->attach($request->libros_select_id[$i]) ;
+                        // return $request;
+                        //Update realizado en la tabla libros (se sumaron la cantidad que ingreso de este libro)
+            
+                        $libro = Libro::find($request->libros_select_id[$i]);
+                        
+                        if($libro->stock_fantasma > 0 && $libro->stock_fantasma > $request->cantidad[$i]  || $libro->stock_fantasma == $request->cantidad[$i] ){
+                            $movimiento->save();
+                            // $movimiento->libros->sync($request->libros_select_id[$i]);
+                            $movimiento->libro()->attach($request->libros_select_id[$i]); //Para poder entrar a una tabla intermedia
+                            $libro->stock_fantasma -= $request->cantidad[$i]; //Aqui sumamos tambien asumamos el stock de llibro para que se sumen a los prestados
+                            $libro->update();
+                            
+                        }else{
+                            return redirect(route('movimientos.index'))->with('error','No contamos con ejemplares disponibles!'); 
+                        }                       
                     }else{
-                        return redirect(route('movimientos.create'))->with('error','No contamos con ejemplares disponibles!'); 
+                        return redirect(route('movimientos.index'))->with('error','Este Lector alcanzo su limite de prestamos estipulado en el sistema!');
                     }
-                }
-                
-                
+                    
+
+                }   
            }
+           
            return redirect(route('movimientos.index'))->with('success','Prestamo nuevo guardado con exito!'); 
-        // }catch(Exception $e){
-        //     return redirect(route('movimientos.index'))->with('error','Cargue correctamente los datos o cargue un estado en el sistema!'); 
-        // }
+        }catch(Exception $e){
+            return redirect(route('movimientos.index'))->with('error','Cargue correctamente los datos o cargue un estado en el sistema!'); 
+        }
         
     }
 
@@ -167,7 +183,9 @@ class MovimientoController extends Controller
                 // $movimiento->cantidad = $request->cantidad;
                 // // $date = Carbon::create($request->fecha_prestamo)->addWeek()->format('Y-m-d'); 
                 // $movimiento->fecha_devolucion = Carbon::create($request->fecha_prestamo)->addWeek()->format('Y-m-d'); 
+               
                 $lector = Lector::find($request->lector_id);
+
                 // return $request->lector_id;
                 $movimiento->fecha_devolucion_real = $request->fecha_devolucion_real;  
     
@@ -203,7 +221,9 @@ class MovimientoController extends Controller
                     }
                 }
                 $movimiento->save();   
+
                 $lector->contador += 1;
+                $lector->cantidadPrestamo -= 1;
                 $lector->update();
                 
                 //Update realizado en la tabla libros (se sumaron la cantidad que ingreso de este libro)
---
+++ app/Http/Controllers/PerdonController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Perdon;
+use App\Lector;
+use Illuminate\Http\Request;
+
+class PerdonController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $perdon = Perdon::all();
+        $lectores = Lector::all() ;
+        return view('perdons.index', compact('perdon', 'lectores'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $perdon = Perdon::all();
+        $lectores = Lector::all() ;
+        return view('perdons.create', compact('perdon', 'lectores'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = request()->validate([
+            'fecha_perdon'=> 'required',
+            'lector_id.*'=> 'required',
+            'descripcion'=> 'required',
+        ]) ;
+
+        $perdon = new Perdon();
+        $perdon->fecha_perdon = $request->fecha_perdon;  
+        $perdon->lector_id = $request->lector_id;
+        $perdon->descripcion = $request->descripcion;
+        $perdon->save();
+
+        $lector = Lector::find($request->lector_id);
+        $lector->reputacion = 50;
+        $lector->update();    
+        
+        return redirect(route('perdons.index'))->with('success','Lector perdonado con exito!'); 
+        
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
